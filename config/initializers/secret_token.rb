354a817188d5058be32b88d6b70d32a1942953fe
senzo <h.jloup@gmail.com>
2011-02-07 18:00:33 +0100

    initialisation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dbz::Application.config.secret_token = 'df0004cce5af22d6f9082ce1fd76d5d570c3e3a2cada98c43b16a32620728b3bdf0fb627f4e173a8031a1c57f112a8dd13f7845386b855831ae940e4b285e588'
