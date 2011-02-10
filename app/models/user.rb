582ff58ee4969ba5c25e565080ea4d7fbb8ac03e
senzo <h.jloup@gmail.com>
2011-02-10 17:12:05 +0100

    creation de la table user + rspec

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+# Schema version: 20110210151454
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+
+  attr_accessible :name, :email
+
+  validates :name, :presence => true
+
+end
