c84fa5ab616fecca790629963e7e7425bdf6dc36
senzo <h.jloup@gmail.com>
2011-02-11 17:24:54 +0100

    mdp cryptee + salt

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,14 +12,52 @@
 
 class User < ActiveRecord::Base
 
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
+
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :name,  :presence => true,
-                    :length   => { :maximum => 50 , :minimum => 3 }
+                    :length   => { :within => 3..50 }
   validates :email, :presence   => true,
                     :format     => { :with => email_regex },
                     :uniqueness => { :case_sensitive => false }
+  validates :password, :presence     => true,
+                       :confirmation => true,
+                       :length       => { :within => 6..40 }
+
+
+  before_save :encrypt_password
+ 
+ def has_password?(submitted_password)
+    # Compare encrypted_password with the encrypted version of submitted_password.
+    encrypted_password == encrypt(submitted_password)
+ end
+
+  before_save :encrypt_password
+
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+
+  private
+
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 
 end
