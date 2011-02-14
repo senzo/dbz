870535754efaafa3a7d79e673e3536ae5f66d299
senzo <h.jloup@gmail.com>
2011-02-14 17:51:20 +0100

    pb css gravatar + debut New User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   def new
     @title = "Sign up"
+    @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
+    @title = @user.name
   end
 
 end
