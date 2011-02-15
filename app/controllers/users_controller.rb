9371cab210f566dfc06118dd274fe16322377a1d
senzo <h.jloup@gmail.com>
2011-02-15 11:46:48 +0100

    ajout erreur dans page new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,15 @@ class UsersController < ApplicationController
     @title = @user.name
   end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save.
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
+
+
 end
