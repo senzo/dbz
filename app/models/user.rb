531ea6e15a079499fbc91d68d12757b2d0224559
senzo <h.jloup@gmail.com>
2011-02-16 15:47:23 +0100

    cookie + secure authantification + creation fini

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,6 +47,13 @@ class User < ActiveRecord::Base
     return user if user.has_password?(submitted_password)
   end
 
+  def self.authenticate_with_salt(id, cookie_salt)
+    user = find_by_id(id)
+    return nil  if user.nil?
+    return user if user.salt == cookie_salt
+  end
+
+
   private
 
   def encrypt_password
